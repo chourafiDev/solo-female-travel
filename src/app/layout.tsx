4ced9e4f33e93bcb418a229ec73025747710b889
chourafiDev <chourafidev@gmail.com>
2025-12-09 20:52:30 +0100

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,28 +64,7 @@ export default function RootLayout({
 				{/* Pinterest Domain Verification - Add your code after verification */}
 				{/* <meta name="p:domain_verify" content="your-pinterest-verification-code" /> */}
 
-				{/* Favicon - Multiple sizes for better support */}
-				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-
-				{/* Android/Chrome */}
-				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="apple-mobile-web-app-title" content="MyWebSite" />
 
 				{/* Microsoft Tiles */}
 				<meta
